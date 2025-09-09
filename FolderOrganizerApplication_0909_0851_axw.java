c2e460df790f59713c8d2dfe14516b2e1b14c219
WAYnEHuDSon478 <wasted-revises-0y@icloud.com>
2025-09-09 09:51:13 +0900

    Build 文件夹结构整理器 using MICRONAUT

---
+++ FolderOrganizerApplication_0909_0851_axw.java
@@ -0,0 +1,13 @@
+// 代码生成时间: 2025-09-09 08:51:11
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.core.annotation.ReflectiveAccess;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+import javax.inject.Singleton;
+
+// 使用@ReflectiveAccess注解允许反射访问私有成员
+@ReflectiveAccess
+@Requires(env = 
